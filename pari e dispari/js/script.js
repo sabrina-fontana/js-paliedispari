77725c22dd3cc8e3ef4952b265fefbdae9ae775f
sabrina-fontana <sabfon@hotmail.it>
2021-01-07 15:14:43 +0100

    pari o dispari completo

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -14,36 +14,40 @@ function pariOrNot(num1, num2) {
     return false;
   }
 }
+var text = document.getElementById('text');
+var result = document.getElementById('result');
+
 // chiedo all'utente di scegliere tra pari e dispari
-var sceltaUtente = prompt('Scegli pari o dispari');
+var sceltaUtente = prompt('Scegli pari o dispari').toLowerCase();
 // controllo l'input dell'utente
 if (sceltaUtente !== 'pari' && sceltaUtente !== 'dispari') {
   alert('Inserisci un valore corretto');
 } else {
-  console.log('Hai scelto ' + sceltaUtente);
+  text.innerHTML += 'Hai scelto ' + sceltaUtente + '<br>';
   // chiedo all'utente di scegliere un numero tra 1 e 5
   var numeroUtente = prompt('Scegli un numero da 1 a 5');
   // controllo il numero inserito
   if (isNaN(numeroUtente) || numeroUtente < 1 || numeroUtente > 5 || numeroUtente.length > 1) {
     alert('Inserisci un numero da 1 a 5');
+    text.innerHTML = '';
   } else {
-    console.log('Il tuo numero è ' + numeroUtente);
+    text.innerHTML += 'Il tuo numero è ' + numeroUtente + '<br>';
     // il computer genera un numero randomico
     var numeroComputer = randomNumber(1, 5);
-    console.log('Il numero del computer è ' + numeroComputer);
+    text.innerHTML += 'Il numero del computer è ' + numeroComputer + '<br>';
     // calcolo se la somma dei due numeri è pari o dispari
     if (pariOrNot(numeroUtente, numeroComputer)) {
-      console.log('la somma è pari');
+      text.innerHTML += 'la somma è pari'  + '<br>';
     } else {
-      console.log('la somma è dispari');
+      text.innerHTML += 'la somma è dispari'  + '<br>';
     }
     // verifico chi ha vinto
     if (sceltaUtente === 'pari' && pariOrNot(numeroUtente, numeroComputer) === true) {
-      console.log('Bravo, hai vinto');
+      result.innerHTML = 'Bravo, hai vinto';
     } else if (sceltaUtente === 'dispari' && pariOrNot(numeroUtente, numeroComputer) === false) {
-      console.log('Bravo, hai vinto');
+      result.innerHTML = 'Bravo, hai vinto';
     } else {
-      console.log('Hai perso');
+      result.innerHTML = 'Hai perso';
     }
   }
 }
